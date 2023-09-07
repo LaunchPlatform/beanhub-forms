0662ab5a4bb10e2301d26d50da358aab0043eece
Fang-Pen Lin <hello@fangpenlin.com>
2023-09-06 22:35:07 -0700

    Add JS

--- beanhub_forms/app/routes.py
+++ beanhub_forms/app/routes.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
 from fastapi import Request
 from fastapi import Response
+from fastapi import status
 
 from . import deps
+from ..form import make_custom_form
+from .helpers import convert_fields_for_js
 
 router = APIRouter()
 
@@ -27,6 +31,32 @@ async def submit_form(
     request: Request,
     templates: deps.Jinja2TemplatesDep,
     flash: deps.FlashDep,
+    form_doc: deps.FormDocDep,
     form_name: str,
 ) -> Response:
-    pass
+    if form_doc is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    forms = {form.name: form for form in form_doc.forms}
+    form_schema = forms.get(form_name)
+    if form_schema is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    CustomForm = make_custom_form(
+        form_schema=form_schema,
+        accounts=[],
+        currencies=[],
+        files=[],
+    )
+    form = await CustomForm.from_formdata(request)
+    fields = convert_fields_for_js(form=form, form_schema=form_schema)
+    if await form.validate_on_submit():
+        pass
+    return templates.TemplateResponse(
+        "form.html",
+        dict(
+            request=request,
+            form_schema=form_schema,
+            form=form,
+            fields=fields,
+        ),
+    )
